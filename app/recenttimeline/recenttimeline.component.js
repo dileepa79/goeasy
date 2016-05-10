617c1fa0030a8dfe88b679df5bf531cd74a2347f
djy <dnirosh@gmail.com>
2016-05-10 11:17:01 +0530

    release fix

--- app/recenttimeline/recenttimeline.component.js
+++ app/recenttimeline/recenttimeline.component.js
@@ -1,94 +1,123 @@
-"use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var core_1 = require('angular2/core');
-var recenttimeline_service_1 = require('../services/recenttimeline.service');
-var timeline_watch_service_1 = require('../services/timeline-watch.service');
-var timeline_component_1 = require('../timeline/timeline.component');
-var sharetimeline_component_1 = require('../sharetimeline/sharetimeline.component');
-var ng2_bs3_modal_1 = require('ng2-bs3-modal/ng2-bs3-modal');
-var RecentTimeLineComponent = (function () {
-    function RecentTimeLineComponent(_timeLineService, _timeLineWatchService, _router) {
-        this._timeLineService = _timeLineService;
-        this._timeLineWatchService = _timeLineWatchService;
-        this._router = _router;
-        // @ViewChild('parentModal')
-        //parentModal: ModalComponent;
-        this.animation = true;
-        this.keyboard = true;
-        this.backdrop = true;
-        this.title = "RECENT TIMELINE";
-        this.timeLineWatch = {
-            tags: [],
-            isWatched: false,
-            isStatusUpdate: false
-        };
-        this.tags = '';
-    }
-    RecentTimeLineComponent.prototype.ngOnInit = function () {
-        this.getRecentTimelines();
+System.register(['angular2/core', '../services/recenttimeline.service', '../services/timeline-watch.service', '../timeline/timeline.component', '../sharetimeline/sharetimeline.component', 'angular2/router', 'ng2-bs3-modal/ng2-bs3-modal'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
     };
-    RecentTimeLineComponent.prototype.getRecentTimelines = function () {
-        var _this = this;
-        this._timeLineService.getRecentTimeLines()
-            .subscribe(function (timelines) {
-            _this.recentTimelines = timelines;
-            console.log(_this.recentTimelines);
-        }, function (error) {
-            _this.errorMessage = error,
-                console.log(_this.errorMessage);
-        }, function () { return function () { return console.log("Done"); }; });
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    RecentTimeLineComponent.prototype.updateTimelineWatch = function (timeLineWatch, selectedTimeline) {
-        var _this = this;
-        this._timeLineWatchService.updateTimelineWatch(timeLineWatch)
-            .subscribe(function (watch) {
-            _this.timeLineWatch = watch;
-            var selected = _this.recentTimelines.filter(function (obj) {
-                return obj.id == selectedTimeline.id;
-            });
-            selected[0].isWatched = _this.timeLineWatch.isWatched;
-        }, function (error) {
-            _this.errorMessage = error,
-                console.log(_this.errorMessage);
-        }, function () { return function () { return console.log("Done"); }; });
-    };
-    RecentTimeLineComponent.prototype.select = function (selectedTimeline) {
-        for (var i = 0; i < selectedTimeline.tags.length; i++) {
-            this.tags = this.tags + (selectedTimeline.tags[i].name + (selectedTimeline.tags.length != i + 1 ? ',' : ''));
+    var core_1, recenttimeline_service_1, timeline_watch_service_1, timeline_component_1, sharetimeline_component_1, router_1, ng2_bs3_modal_1;
+    var RecentTimeLineComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (recenttimeline_service_1_1) {
+                recenttimeline_service_1 = recenttimeline_service_1_1;
+            },
+            function (timeline_watch_service_1_1) {
+                timeline_watch_service_1 = timeline_watch_service_1_1;
+            },
+            function (timeline_component_1_1) {
+                timeline_component_1 = timeline_component_1_1;
+            },
+            function (sharetimeline_component_1_1) {
+                sharetimeline_component_1 = sharetimeline_component_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
+            },
+            function (ng2_bs3_modal_1_1) {
+                ng2_bs3_modal_1 = ng2_bs3_modal_1_1;
+            }],
+        execute: function() {
+            RecentTimeLineComponent = (function () {
+                function RecentTimeLineComponent(_timeLineService, _timeLineWatchService, _router) {
+                    this._timeLineService = _timeLineService;
+                    this._timeLineWatchService = _timeLineWatchService;
+                    this._router = _router;
+                    // @ViewChild('parentModal')
+                    //parentModal: ModalComponent;
+                    this.animation = true;
+                    this.keyboard = true;
+                    this.backdrop = true;
+                    this.title = "RECENT TIMELINE";
+                    this.timeLineWatch = {
+                        tags: [],
+                        isWatched: false,
+                        isStatusUpdate: false
+                    };
+                    this.tags = '';
+                }
+                RecentTimeLineComponent.prototype.ngOnInit = function () {
+                    this.getRecentTimelines();
+                };
+                RecentTimeLineComponent.prototype.getRecentTimelines = function () {
+                    var _this = this;
+                    this._timeLineService.getRecentTimeLines()
+                        .subscribe(function (timelines) {
+                        _this.recentTimelines = timelines;
+                        console.log(_this.recentTimelines);
+                    }, function (error) {
+                        _this.errorMessage = error,
+                            console.log(_this.errorMessage);
+                    }, function () { return function () { return console.log("Done"); }; });
+                };
+                RecentTimeLineComponent.prototype.updateTimelineWatch = function (timeLineWatch, selectedTimeline) {
+                    var _this = this;
+                    this._timeLineWatchService.updateTimelineWatch(timeLineWatch)
+                        .subscribe(function (watch) {
+                        _this.timeLineWatch = watch;
+                        var selected = _this.recentTimelines.filter(function (obj) {
+                            return obj.id == selectedTimeline.id;
+                        });
+                        selected[0].isWatched = _this.timeLineWatch.isWatched;
+                    }, function (error) {
+                        _this.errorMessage = error,
+                            console.log(_this.errorMessage);
+                    }, function () { return function () { return console.log("Done"); }; });
+                };
+                RecentTimeLineComponent.prototype.select = function (selectedTimeline) {
+                    for (var i = 0; i < selectedTimeline.tags.length; i++) {
+                        this.tags = this.tags + (selectedTimeline.tags[i].name + (selectedTimeline.tags.length != i + 1 ? ',' : ''));
+                    }
+                    this._router.navigate(['TimeLine', { tags: this.tags }]);
+                };
+                RecentTimeLineComponent.prototype.openModal = function (timeline) {
+                    this.selectedTimeline = timeline;
+                    //this.parentModal.open()
+                };
+                RecentTimeLineComponent.prototype.watch = function (selectedTimeline) {
+                    var timeLineWatch = {
+                        tags: [],
+                        isWatched: false,
+                        isStatusUpdate: false
+                    };
+                    timeLineWatch.isWatched = selectedTimeline.isWatched;
+                    timeLineWatch.isStatusUpdate = true;
+                    timeLineWatch.tags = selectedTimeline.tags.map(function (d) { return d['name']; });
+                    this.updateTimelineWatch(timeLineWatch, selectedTimeline);
+                };
+                RecentTimeLineComponent = __decorate([
+                    core_1.Component({
+                        selector: 'recentimeline',
+                        templateUrl: './app/recenttimeline/recenttimeline.component.html',
+                        providers: [
+                            recenttimeline_service_1.RecentTimeLineService, timeline_watch_service_1.TimeLineWatchService
+                        ],
+                        directives: [timeline_component_1.TimeLineComponent, ng2_bs3_modal_1.MODAL_DIRECTIVES, sharetimeline_component_1.ShareTimelineComponent]
+                    }), 
+                    __metadata('design:paramtypes', [recenttimeline_service_1.RecentTimeLineService, timeline_watch_service_1.TimeLineWatchService, router_1.Router])
+                ], RecentTimeLineComponent);
+                return RecentTimeLineComponent;
+            }());
+            exports_1("RecentTimeLineComponent", RecentTimeLineComponent);
         }
-        this._router.navigate(['TimeLine', { tags: this.tags }]);
-    };
-    RecentTimeLineComponent.prototype.openModal = function (timeline) {
-        this.selectedTimeline = timeline;
-        //this.parentModal.open()
-    };
-    RecentTimeLineComponent.prototype.watch = function (selectedTimeline) {
-        var timeLineWatch = {
-            tags: [],
-            isWatched: false,
-            isStatusUpdate: false
-        };
-        timeLineWatch.isWatched = selectedTimeline.isWatched;
-        timeLineWatch.isStatusUpdate = true;
-        timeLineWatch.tags = selectedTimeline.tags.map(function (d) { return d['name']; });
-        this.updateTimelineWatch(timeLineWatch, selectedTimeline);
-    };
-    RecentTimeLineComponent = __decorate([
-        core_1.Component({
-            selector: 'recentimeline',
-            templateUrl: './app/recenttimeline/recenttimeline.component.html',
-            providers: [
-                recenttimeline_service_1.RecentTimeLineService, timeline_watch_service_1.TimeLineWatchService
-            ],
-            directives: [timeline_component_1.TimeLineComponent, ng2_bs3_modal_1.MODAL_DIRECTIVES, sharetimeline_component_1.ShareTimelineComponent]
-        })
-    ], RecentTimeLineComponent);
-    return RecentTimeLineComponent;
-}());
-exports.RecentTimeLineComponent = RecentTimeLineComponent;
+    }
+});
 //# sourceMappingURL=recenttimeline.component.js.map
--- app/sharetimeline/sharetimeline.component.js
+++ app/sharetimeline/sharetimeline.component.js
@@ -1,48 +1,68 @@
-"use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var core_1 = require('angular2/core');
-var tags_service_1 = require('../services/tags.service');
-var users_selector_component_1 = require('../noteshareusers/users-selector.component');
-var ShareTimelineComponent = (function () {
-    function ShareTimelineComponent(tagService) {
-        this.tagService = tagService;
-        this.users = [];
-    }
-    ShareTimelineComponent.prototype.filterUserMultiple = function (event) {
-        var _this = this;
-        var query = event.query;
-        this.tagService.getTags().then(function (users) {
-            _this.filteredUsersMultiple = _this.filtertag(query, users);
-        });
+System.register(['angular2/core', '../services/tags.service', '../noteshareusers/users-selector.component'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
     };
-    ShareTimelineComponent.prototype.filtertag = function (query, users) {
-        //in a real application, make a request to a remote url with the query and return filtered results, for demo we filter at client side
-        var filtered = [];
-        for (var i = 0; i < users.length; i++) {
-            var user = users[i];
-            if (user.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
-                filtered.push(user);
-            }
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1, tags_service_1, users_selector_component_1;
+    var ShareTimelineComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (tags_service_1_1) {
+                tags_service_1 = tags_service_1_1;
+            },
+            function (users_selector_component_1_1) {
+                users_selector_component_1 = users_selector_component_1_1;
+            }],
+        execute: function() {
+            ShareTimelineComponent = (function () {
+                function ShareTimelineComponent(tagService) {
+                    this.tagService = tagService;
+                    this.users = [];
+                }
+                ShareTimelineComponent.prototype.filterUserMultiple = function (event) {
+                    var _this = this;
+                    var query = event.query;
+                    this.tagService.getTags().then(function (users) {
+                        _this.filteredUsersMultiple = _this.filtertag(query, users);
+                    });
+                };
+                ShareTimelineComponent.prototype.filtertag = function (query, users) {
+                    //in a real application, make a request to a remote url with the query and return filtered results, for demo we filter at client side
+                    var filtered = [];
+                    for (var i = 0; i < users.length; i++) {
+                        var user = users[i];
+                        if (user.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+                            filtered.push(user);
+                        }
+                    }
+                    return filtered;
+                };
+                ShareTimelineComponent.prototype.onSelectedUsersChanged = function (users) {
+                    this.users = users.map(function (d) { return d['userName']; });
+                };
+                ShareTimelineComponent = __decorate([
+                    core_1.Component({
+                        selector: 'sharetimeline',
+                        directives: [users_selector_component_1.UsersSelectorComponent],
+                        templateUrl: './app/sharetimeline/sharetimeline.component.html',
+                        providers: [tags_service_1.TagsService]
+                    }), 
+                    __metadata('design:paramtypes', [tags_service_1.TagsService])
+                ], ShareTimelineComponent);
+                return ShareTimelineComponent;
+            }());
+            exports_1("ShareTimelineComponent", ShareTimelineComponent);
         }
-        return filtered;
-    };
-    ShareTimelineComponent.prototype.onSelectedUsersChanged = function (users) {
-        this.users = users.map(function (d) { return d['userName']; });
-    };
-    ShareTimelineComponent = __decorate([
-        core_1.Component({
-            selector: 'sharetimeline',
-            directives: [users_selector_component_1.UsersSelectorComponent],
-            templateUrl: './app/sharetimeline/sharetimeline.component.html',
-            providers: [tags_service_1.TagsService]
-        })
-    ], ShareTimelineComponent);
-    return ShareTimelineComponent;
-}());
-exports.ShareTimelineComponent = ShareTimelineComponent;
+    }
+});
 //# sourceMappingURL=sharetimeline.component.js.map
