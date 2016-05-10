cd0186128fefc56eb8ba7342b3a93d365ae34926
djy <dnirosh@gmail.com>
2016-05-10 11:00:21 +0530

    fix release

--- app/app.constants.js
+++ app/app.constants.js
@@ -20,8 +20,8 @@ System.register(['angular2/core'], function(exports_1, context_1) {
         execute: function() {
             Configuration = (function () {
                 function Configuration() {
-                    //public Server: string = "http://yaycollaborationapi.azurewebsites.net/";
-                    this.Server = "http://localhost:54736/";
+                    this.Server = "http://yaycollaborationapi.azurewebsites.net/";
+                    //public Server: string = "http://localhost:54736/";
                     this.ApiUrl = "api/";
                     this.ServerWithApiUrl = this.Server + this.ApiUrl;
                 }
